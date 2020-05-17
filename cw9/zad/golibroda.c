6a98bab81ff3c705c0e86d8abd23b1b4e3b39ce5
PKopel <pawel.kopel2@gmail.com>
2020-05-18 00:19:05 +0200

    ready cw9

--- cw9/zad/golibroda.c
+++ cw9/zad/golibroda.c
@@ -3,62 +3,102 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
 #include <errno.h>
 
 typedef struct client_t{
     pthread_t id;
-    client_t* next;
+    struct client_t* next;
 } client_t;
 
 typedef struct client_q{
     client_t* head;
     client_t* last;
     client_t* cabinet;
-    int aviable;
+    int clients_waiting;
 } client_q;
 
-client_q* clients = NULL;
+int max_clients;
+client_q* clients;
 pthread_mutex_t clients_mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t clients_cond = PTHREAD_COND_INITIALIZER;
+
+
+void error(char* msg){
+    perror(msg);
+    exit(errno);
+}
 
 void* client(void* arg){
     client_t* new_client = 
         (client_t*)malloc(sizeof(client_t));
     char* msg = 
-        (char*)calloc(40, sizeof(char));
+        (char*)malloc(40*sizeof(char));
     new_client->id = pthread_self();
-    pthread_mutex_lock(&clients_mtx);
-    if(clients->cabinet == NULL){
+    try_again: pthread_mutex_lock(&clients_mtx);
+    if(clients->cabinet == NULL && clients->last == NULL){
         clients->cabinet = new_client;
         pthread_cond_signal(&clients_cond);
-        sprintf(msg,
-            "Budze golibrode %ld",
+        printf("Budze golibrode %ld\n",
             new_client->id);
-    } else if(clients->aviable > 0){
-        new_client->next = clients->last;
+    } else if(clients->clients_waiting < max_clients){
+        if(clients->last != NULL) clients->last->next = new_client;
         clients->last = new_client;
-        pthread_cond_signal(&clients_cond);
-        sprintf(msg,
-            "Poczekalnie, wolne miejsca: %d; %ld",
-            --clients->aviable,
+        ++clients->clients_waiting;
+        if( clients->head == NULL) clients->head = new_client;
+        printf("Poczekalnia, wolne miejsca: %d; %ld\n",
+            max_clients - clients->clients_waiting,
             new_client->id);
     } else {
-        sprintf(msg,"Zajete; %ld", new_client->id);
+        printf("Zajete; %ld\n", new_client->id);
+        int sleep_duration = 2+rand()%9;
+        pthread_mutex_unlock(&clients_mtx);
+        sleep(sleep_duration);
+        goto try_again;
     }
     pthread_mutex_unlock(&clients_mtx);
-    printf("%s\n", msg);
-    free((void*)msg);
+    return NULL;
 }
 
 void* barber(void* arg){
-    pthread_mutex_lock(&clients_mtx);
-    while (/* condition */)
-    {
-        /* code */
+    int sleep_duration, number_of_clients = (int)arg;
+    while (number_of_clients--){
+        pthread_mutex_lock(&clients_mtx);
+        while (clients->head == NULL && clients->cabinet == NULL){
+            printf("Golibroda: ide spac\n");
+            pthread_cond_wait(&clients_cond,&clients_mtx);
+        }
+        if(clients->cabinet == NULL){
+            clients->cabinet = clients->head;
+            clients->head = clients->head->next;
+            clients->cabinet->next = NULL;
+            clients->clients_waiting--;
+        }
+        printf("Golibroda: czeka %d klientow, gole klienta %ld\n", 
+            clients->clients_waiting,
+            clients->cabinet->id);
+        pthread_mutex_unlock(&clients_mtx);
+        sleep_duration = 3+rand()%8;
+        sleep(sleep_duration);
+        pthread_mutex_lock(&clients_mtx);
+        free((void*)clients->cabinet);
+        clients->cabinet = NULL;
+        pthread_mutex_unlock(&clients_mtx);
     }
-    
+    return NULL;
 }
 
 int main(int argc, char** argv){
-
+    srand(time(NULL));
+    max_clients = atoi(argv[1]);
+    int number_of_clients = atoi(argv[2]);
+    pthread_t new_thread, barber_id;
+    clients = (client_q*)calloc(1,sizeof(client_q));
+    if((errno = pthread_create(&barber_id, NULL, barber, (void*)number_of_clients)!= 0)) error("barber create");
+    for(int i = 0; i < number_of_clients; i++){
+        if((errno = pthread_create(&new_thread, NULL, client, NULL)!= 0)) error("client create");
+        sleep(rand()%10);
+    }
+    pthread_join(barber_id,NULL);
 }
